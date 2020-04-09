d00f48985d948f48111c04df39f99ad9185b14cd
DevinNorgarb <dnorgarb@gmail.com>
2020-04-09 07:35:58 +0200

    wip

---
+++ src/pages/Profile.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+    <div v-if="profile.id">
+      <div class="q-title"></div>
+      <p><img :src="profile.picture" ></p>
+    </div>
+  </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  data () {
+    return {
+      profile: {}
+    }
+  },
+  mounted () {
+    this.getProfile('facebook')
+  },
+  methods: {
+    getProfile (network) {
+      // if (this.$hello.getAuthResponse(network) == null) {
+      //   return
+      // }
+      // this.$hello(network).api('me')
+      //   .then((res) => {
+      //     console.log(res)
+      //     this.profile = res
+      //   })
+    }
+  }
+}
+</script>
